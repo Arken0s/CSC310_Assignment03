d39678a9589f0b3e75d6761379824d90c298a0ca
Jakeobie <jacob.m.obryan@gmail.com>
2019-10-21 09:42:57 -0700

    Update

--- src/Assignment03.java
+++ src/Assignment03.java
@@ -1,21 +1,50 @@
+import javax.sound.midi.Soundbank;
 import java.util.Random;
 
 public class Assignment03 {
 
+    private static int recursionCount = 0;
+
     public static void main(String[] args) {
-        //int[] testArray = RandomArray(10);
-        int[] testArray = new int[]{5,7,8,1,2,9};
+        //************************
+        //** MERGE SORT TESTING **
+        //************************
+
+//        int[] testArray = RandomArray(10);
+        int[] testArray = new int[]{16,7,10,1,5,11,3,8,14,4,2,12,6,13,9,15};
         System.out.println("Test Array before merge sort: ");
         PrintArray(testArray);
+//        MergeSort(testArray, 0, 15);
+        System.out.println("TestArrayLength - 1 = " + (testArray.length - 1));
         MergeSort(testArray, 0, (testArray.length - 1));
         System.out.println("Test Array after merge sort: ");
         PrintArray(testArray);
+
+        //************************
+        //** QUICK SORT TESTING **
+        //************************
+//        int[] testArray = new int[]{16,7,10,1,5,11,3,8,14,4,2,12,6,13,9,15};
+//        System.out.println("Test Array before quick sort: ");
+//        PrintArray(testArray);
+//        QuickSort(testArray, 0, (testArray.length - 1));
+//        System.out.println("Test Array after quick sort: ");
+//        PrintArray(testArray);
     }
 
-    public static void MergeSort(int[] dataList, int firstIndex, int lastIndex) { //TODO: Rewrite to have the firstIndex and lastIndex generated from dataList.length.
-        if (firstIndex < lastIndex) { // 0 - 5
+    /*
+                                                                     **************************************
+                                                                     ****          MERGESORT           ****
+                                                                     **************************************
+     */
+    public static void MergeSort(int[] dataList, int firstIndex, int lastIndex) {
+        System.out.println("*********** Recursion Count: " + recursionCount + " ***********");
+        System.out.println("First Index = "+ firstIndex + ". Last Index = " + lastIndex);
+        System.out.println("First Index Value = "+ dataList[firstIndex] + ". Last Index Value = " + dataList[lastIndex]);
+        recursionCount++;
+        if (firstIndex < lastIndex) {
+            System.out.println("Loop was ran");
             int middleIndex = ((firstIndex + lastIndex) / 2);
-            System.out.println("Middle Index: " +  middleIndex);
+            System.out.println("Middle Index: " +  middleIndex); //DEBUG
             MergeSort(dataList, firstIndex, middleIndex);
             MergeSort(dataList, middleIndex + 1, lastIndex);
             MergeLists(dataList, firstIndex, middleIndex, middleIndex +1, lastIndex);
@@ -23,6 +52,8 @@ public class Assignment03 {
     }
 
     public static void MergeLists(int[] dataList, int firstIndex, int middleIndex, int midplusoneIndex, int lastIndex) {
+        System.out.println("MergeLists Running with. firstIndex: " + firstIndex + " middleIndex : " + middleIndex + " midplusoneIndex: "+ midplusoneIndex + " lastIndex: " + lastIndex);
+        System.out.println("MergeLists Running with. firstIndex Value: " + dataList[firstIndex] + " middleIndex Value : " + dataList[middleIndex] + " midplusoneIndex: Value "+ dataList[midplusoneIndex] + " lastIndex Value: " + dataList[lastIndex]);
         int start1 = firstIndex;
         int end1 = middleIndex;
         int start2 = midplusoneIndex;
@@ -42,24 +73,59 @@ public class Assignment03 {
             indexC += 1;
         }
         if (start1 <= end1) {
-            for (int i = start1; i > end1; i++) {
+            for (int i = start1; i <= end1; i++) {
                 resultList[indexC] = dataList[i];
                 indexC += 1;
             }
         } else {
-            for (int i = start2; i > end2; i++) {
+            for (int i = start2; i <= end2; i++) {
                 resultList[indexC] = dataList[i];
                 indexC += 1;
             }
         }
         indexC = 0;
-        for (int i = finalStart; i > finalEnd; i++) {
+        for (int i = finalStart; i <= finalEnd; i++) {
             dataList[i] = resultList[indexC];
             indexC += 1;
         }
         System.out.println("Result Array at end of MergeLists:");
         PrintArray(resultList);
+        System.out.println("Test Array at end of MergeLists:");
+        PrintArray(dataList);
+        System.out.println(dataList[0]);
     }
+
+    /*
+                                                                    **************************************
+                                                                    ****          QUICKSORT           ****
+                                                                    **************************************
+    */
+    public static void QuickSort(int[] dataList, int first, int last) {
+        if (first < last) {
+            int pivot = PivotList(dataList, first, last);
+            QuickSort(dataList, first, pivot-1);
+            QuickSort(dataList, pivot + 1, last);
+        }
+    }
+
+    public static int PivotList(int[] dataList, int first, int last) {
+        int pivotValue = dataList[first];
+        int pivotPoint = first;
+        for (int i = (first+1); i <= (dataList.length - 1); i++) {
+            if (dataList[i] < pivotValue) {
+                pivotPoint = pivotPoint + 1;
+                SwapElements(dataList, pivotPoint, i);
+            }
+        }
+        SwapElements(dataList, first, pivotPoint);
+        return pivotPoint;
+    }
+
+/*
+                                                                     **************************************
+                                                                     ****      SUPPORT FUNCTIONS       ****
+                                                                     **************************************
+ */
 
     public static int[] RandomArray(int listSize) {
         int[] returnList = new int[listSize];
@@ -98,6 +164,12 @@ public class Assignment03 {
             value = value - 1;
         }
         return orderedList;
+    }
+
+    public static void SwapElements(int[] data, int index1, int index2) {
+        int tempValue = data[index1];
+        data[index1] = data[index2];
+        data[index2] = tempValue;
     }
 
     public static void PrintArray(int[] array) {
